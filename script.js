70d5cc6a0b98cfa1ace461f8f3cc5bfc5813235b
barak <rosnerbb@gmail.com>
2021-03-24 10:19:03 -0400

    Finishing touches

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const h2 = document.createElement('h2');
 h2.textContent = "Thank you for visiting!";
 
 
+
 document.querySelector('body').appendChild(h2);
 
 const hidden = document.querySelector('.hidden');
-/* hidden.addEventListener('click', function() {
+hidden.addEventListener('click', function() {
     alert('You win!')
 })
-*/
+
+
